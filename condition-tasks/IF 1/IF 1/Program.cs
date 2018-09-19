9bd1cb05158b61da158da6d6cc5fdf5b1ef4c743
Jiivatti <jere.vatto@gmail.com>
2018-09-19 09:17:26 +0300

    Everything went wrong. Probably.

---
+++ condition-tasks/IF 1/IF 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IF_1
+{
+    class Program
+    {
+        static void Main(string[] args) //This shit looks hard huh?
+        {
+            Console.WriteLine("Ohjelma selvittää, onko syötetty luku positiivinen, " +
+                "negatiivinen vai nolla.");
+            Console.WriteLine("Syötä luku.");
+                
+        }
+    }
+}/*
+hooo baby what's happening
+*/
