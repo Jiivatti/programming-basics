aa0bcc48b73b733843f7fb32ae5cc3f5713e5bc0
Jiivatti <jere.vatto@gmail.com>
2018-09-19 09:59:36 +0300

    Made a new program. Hacks NSA servers and stuff

--- condition-tasks/IF 1/IF 1/Program.cs
+++ condition-tasks/IF 1/IF 1/Program.cs
@@ -8,10 +8,15 @@ namespace IF_1
         {
             Console.WriteLine("Ohjelma selvittää, onko syötetty luku positiivinen, " +
                 "negatiivinen vai nolla.");
-            Console.WriteLine("Syötä luku.");
-                
+            Console.Write("Syötä numero: ");
+            string userInput = Console.ReadLine(); //"userInput" on osoitettu muuttujaksi
+            int muuttuja = int.Parse(userInput); //"muuttuja" on toinen... muuttuja
+            if (muuttuja == 0)
+                Console.WriteLine($"Numero {muuttuja} on nolla.");
+            else if (muuttuja < 0)
+                Console.WriteLine($"Numero {muuttuja} on negatiivinen.");
+            else
+                Console.WriteLine($"Numero {muuttuja} on positiivinen.");
         }
     }
-}/*
-hooo baby what's happening
-*/
+}
