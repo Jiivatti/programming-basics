b55c705e7ba8df0ac4ce71674338d98bffc3f694
Jiivatti <jere.vatto@gmail.com>
2018-09-20 08:32:47 +0300

    Started IF2 :V

---
+++ condition-tasks/IF2/IF2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IF2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelmaa : D");
+        }
+    }
+}
