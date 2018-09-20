7994b8c9f76eb975a92c41849b3a4a32b8c0254a
Jiivatti <jere.vatto@gmail.com>
2018-09-20 09:26:03 +0300

    Created IF3

---
+++ condition-tasks/IF3/IF3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IF3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selittää onko luku pariton vai parillinen, positiivinen vai negatiivinen.");
+            Console.Write("Syötä luku: ");
+            
+        }
+    }
+}
