348b5fed7782d972224dae203085e0d3551aadaa
Jiivatti <jere.vatto@gmail.com>
2018-09-20 09:16:57 +0300

    Finished IF2

--- condition-tasks/IF2/IF2/Program.cs
+++ condition-tasks/IF2/IF2/Program.cs
@@ -6,7 +6,19 @@ namespace IF2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ohjelmaa : D");
+            Console.WriteLine("Ohjelma selvittää onko luku parillinen vai pariton.");
+            Console.Write("Syötä numero: ");
+            string userInput = Console.ReadLine();
+            int number = 0;
+            bool isNumber = int.TryParse(userInput, out number);
+            if (isNumber)
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine($"Luku {number} on parillinen.");
+                else Console.WriteLine($"Luku {number} on pariton.");
+            }
+            else
+                Console.WriteLine("Väärä syöte >:I");
         }
     }
 }
