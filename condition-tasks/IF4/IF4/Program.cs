504599400c1edb1673c5241ce1df568e44d0181a
Jiivatti <jere.vatto@gmail.com>
2018-09-26 10:48:32 +0300

    Renamed

---
+++ condition-tasks/IF4/IF4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IF4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
