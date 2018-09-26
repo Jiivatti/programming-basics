081b371de7299f451161fef78a677e086095c1a9
Jiivatti <jere.vatto@gmail.com>
2018-09-26 10:41:12 +0300

    Finished the program : D might work

--- condition-tasks/IF3/IF3/Program.cs
+++ condition-tasks/IF3/IF3/Program.cs
@@ -8,7 +8,19 @@ namespace IF3
         {
             Console.WriteLine("Ohjelma selittää onko luku pariton vai parillinen, positiivinen vai negatiivinen.");
             Console.Write("Syötä luku: ");
-            
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            if (number < 0)
+            {if (number % 2 == 0)
+                    Console.WriteLine($"Luku {number} on negatiivinen ja parillinen");
+                else Console.WriteLine($"Luku {number} on negatiivinen ja pariton");
+                        }
+            if (number > 0)
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine($"Luku {number} on positiivinen ja parillinen");
+                else Console.WriteLine($"Luku {number} on positiivinen ja pariton");
+            }
         }
     }
 }
