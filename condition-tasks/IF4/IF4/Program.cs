a4ed4fc4c322e249ab8677b2724e300bd7e9ed0d
Jiivatti <jere.vatto@gmail.com>
2018-09-26 12:35:45 +0300

    IF4 is monumentally screwed D:

--- condition-tasks/IF4/IF4/Program.cs
+++ condition-tasks/IF4/IF4/Program.cs
@@ -6,7 +6,38 @@ namespace IF4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma selvittää kolmen syötetyn luvun korkeusjärjestyksen.");
+            Console.Write("Syötä ensimmäinen luku: ");
+            string userInput = Console.ReadLine();
+            int numberA = int.Parse(userInput);
+            Console.Write("Syötä toinen luku: ");
+            userInput = Console.ReadLine();
+            int numberB = int.Parse(userInput);
+            Console.Write("Syötä kolmas luku: ");
+            userInput = Console.ReadLine();
+            int numberC = int.Parse(userInput);
+            if (numberA > numberB)
+            {
+                if (numberB > numberC)
+                    Console.WriteLine($"Järjestys on {numberA} > {numberB} > {numberC}");
+                else
+                    Console.WriteLine($"Järjestys on {numberC} > {numberA} > {numberB}");
+            }
+            else if (numberB > numberC)
+            {
+                if (numberA > numberC)
+                    Console.WriteLine($"Järjestys on {numberB} > {numberA} > {numberC}");
+                else
+                    Console.WriteLine($"Järjestys on {numberC} > {numberB} > {numberA}");
+            }
+            else if (numberC > numberA)
+            {
+                if (numberB > numberA)
+                    Console.WriteLine($"Järjestys on {numberC} > {numberB} > {numberA}");
+                else
+                    Console.WriteLine($"Järjestys on {numberA} > {numberC} > {numberB}");
+            }
+
         }
     }
 }
