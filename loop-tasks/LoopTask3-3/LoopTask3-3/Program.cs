c840b3ee4d992ac8b4bb0fe4b7f2775167c32467
Jiivatti <jere.vatto@gmail.com>
2018-10-04 10:40:16 +0300

    Finished task 3, started 4.

--- loop-tasks/LoopTask3-3/LoopTask3-3/Program.cs
+++ loop-tasks/LoopTask3-3/LoopTask3-3/Program.cs
@@ -6,7 +6,28 @@ namespace LoopTask3_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma laskee N:n ensimmäisen parittoman ja parillisen luvun summan.");
+            int X = 0;
+            int i = 1;
+            int evenSum = 0;
+            int oddSum = 0;
+
+            Console.Write("Syötä luku: ");
+            X = int.Parse(Console.ReadLine());
+
+            while (i <= X)
+            {
+                if (i % 2 == 0)
+                {
+                    evenSum = evenSum + i;  //evenSum += i; toimii myös
+                }
+                else
+                {
+                    oddSum = oddSum + i;
+                }
+                i++;
+            }
+            Console.WriteLine($"Parillisten summa on {evenSum}, parittomien {oddSum}. ");
         }
     }
 }
