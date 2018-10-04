b83d8715a277189d3f496f23e2a3c64ba5eab286
Jiivatti <jere.vatto@gmail.com>
2018-10-04 09:34:39 +0300

    Finished task 1, started task 2.

---
+++ loop-tasks/LoopTask3-2/LoopTask3-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LoopTask3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee N ensimmäistä numeroa yhteen. ");
+            int X = 0;
+            int i = 1;
+            int sum = 0;
+
+        }
+    }
+}
--- loop-tasks/LoopTasks3-1/LoopTasks3-1/Program.cs
+++ loop-tasks/LoopTasks3-1/LoopTasks3-1/Program.cs
@@ -6,7 +6,27 @@ namespace LoopTasks3_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma laskee annetun luvun kertoman. ");
+            int X = 0;
+            int i = 1;
+            int fact = 1;
+            do
+            {
+                Console.Write("Syötä luku: ");
+                X = int.Parse(Console.ReadLine());
+
+                if (X <= 0)
+                {
+                    Console.WriteLine($"Luku {X} on pienempi kuin 0. Ei kertomaa. ");
+                }
+            } while (X <= 0);
+
+            while(i<=X)
+            {
+                fact = fact * i;   //fact=*i
+                i = i + 1;         //i++
+            }
+            Console.WriteLine($"Luvun {X} kertoma on {fact}");
         }
     }
 }
