e9c6c0f746555a01a110afa4a73a5e999ffc809b
Jiivatti <jere.vatto@gmail.com>
2018-10-04 10:07:32 +0300

    Finished task 2, started task 3

--- loop-tasks/LoopTask3-2/LoopTask3-2/Program.cs
+++ loop-tasks/LoopTask3-2/LoopTask3-2/Program.cs
@@ -11,6 +11,24 @@ namespace LoopTask3_2
             int i = 1;
             int sum = 0;
 
+            do
+            {
+                Console.Write("Syötä luku: ");
+                X = int.Parse(Console.ReadLine());
+
+                if (X <= 0)
+                {
+                    Console.WriteLine("Luvun tulee olla yli 0.");
+                }
+            } while (X <= 0);
+
+            while (i<=X)
+            {
+                sum = sum + i;
+                i = i + 1;
+            }
+            Console.WriteLine($"Tulos on {sum}. ");
+
         }
     }
 }
