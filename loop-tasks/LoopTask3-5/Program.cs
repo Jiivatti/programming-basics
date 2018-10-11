b6def9d756f4b5c9bf0bd496ecb01fdb981d75bb
Jiivatti <jere.vatto@gmail.com>
2018-10-11 09:08:08 +0300

    hurr

---
+++ loop-tasks/LoopTask3-5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LoopTask3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee N:n ensimmäisen parittoman ja parillisen luvun summan.");
+            int X = 0;
+            int i = 0;
+            int evenSum = 0;
+            int oddSum = 0;
+
+            Console.Write("Syötä luku: ");
+            X = int.Parse(Console.ReadLine());
+
+            if (X > 0)
+            {
+                while (i <= X)
+                {
+                    if (i % 2 == 0)
+                    {
+                        evenSum = evenSum + i;  //evenSum += i; toimii myös
+                    }
+                    else
+                    {
+                        oddSum = oddSum + i;
+                    }
+                    i++;
+                }
+            }
+            else
+            {
+                while (i >= X)
+                {
+                    if (i % 2 == 0)
+                    {
+                        evenSum = evenSum + i;
+                    }
+                    else
+                    {
+                        oddSum = oddSum + i;
+                    }
+                    i--;
+                }
+            }
+            Console.WriteLine($"Parillisten tulos on {evenSum}, parittomien {oddSum}. ");
+        }
+    }
+}
