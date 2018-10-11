18d9fe90c48b504e4ed1a8d3be152252974d55d7
Jiivatti <jere.vatto@gmail.com>
2018-10-11 09:00:51 +0300

    Made some loop tasks

--- loop-tasks/LoopTask3-4/LoopTask3-4/Program.cs
+++ loop-tasks/LoopTask3-4/LoopTask3-4/Program.cs
@@ -6,7 +6,28 @@ namespace LoopTask3_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma laskee N ensimmäistä numeroa yhteen. ");
+            int X = 0;
+            int i = 0;
+            int sum = 0;
+
+            Console.Write("Syötä luku: ");
+            X = int.Parse(Console.ReadLine());
+
+            if (X > 0)
+                while (i <= X)
+                {
+                    sum = sum + 1;
+                    i = i + 1;
+                }
+            else
+                while (i >= X)
+                {
+                    sum = sum + 1;
+                    i = i - 1;
+                }
+
+            Console.WriteLine($"Tulos on {sum}");
         }
     }
 }
