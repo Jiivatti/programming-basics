3ff15a4b4cd7028441c51d5c9f7d8dae326f06f8
Jiivatti <Jiivatti>
2018-11-08 13:30:07 +0200

    Fixed the last loop task

--- loop-tasks/Looptask4-5/Program.cs
+++ loop-tasks/Looptask4-5/Program.cs
@@ -9,15 +9,15 @@ namespace Looptask4_5
             Console.WriteLine("Ohjelma arpoo 13-rivisen vakioveikkauksen.");
             Random rnd = new Random();
 
-            for (int i = 0; i < 13; i++)
+            for (int i = 1; i < 14; i++)
             {
                 double muuttuja = rnd.NextDouble();
                 if (muuttuja < 0.4)
-                    Console.WriteLine($"{i}. 1");
-                else if (muuttuja == 0.4 - 0.6)
-                    Console.WriteLine($"{i}. X");
+                    Console.WriteLine($"{i}\t. 1");
+                else if (muuttuja > 0.6)
+                    Console.WriteLine($"{i}\t. X");
                 else
-                    Console.WriteLine($"{i}. 2");
+                    Console.WriteLine($"{i}\t. 2");
 
             }
         }
