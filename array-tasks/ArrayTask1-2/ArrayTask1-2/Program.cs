e084a35be487e414760a5b9844357a6b8a9e0819
Jiivatti <Jiivatti>
2018-11-29 12:01:15 +0200

    Finished array task 2.

---
+++ array-tasks/ArrayTask1-2/ArrayTask1-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArrayTask1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] luvut;
+            luvut = new int[100];
+            double sums = 0;
+            double average = 0;
+
+
+            for (int i = 0; i < luvut.Length; i++)
+            {
+                luvut[i] = rnd.Next(50);
+                sum += luvut[i];
+                Console.WriteLine($"{i + 1}. {luvut[i]}");
+                average = sum / luvut.Length;
+            }
+
+            Console.WriteLine($"Arvottujen lukujen keskiarvo on {average}. ");
+        }
+    }
+}
