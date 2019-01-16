7ab8a0fa5f3c5616d9a32d03568fe57e71ea9075
Jiivatti <Jiivatti>
2019-01-16 11:18:00 +0200

    Lottery program created. Still missing extra & doubling numbers.

---
+++ array-tasks/Lotto-ohjelma/Lotto-ohjelma/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lotto-ohjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            int tmp1, tmp2;
+            int i = 0;
+            Random rng = new Random();
+
+            for (i = 0; i < array.Length; i++)
+            {
+                array[i] = rng.Next(1, 40);
+            }
+
+            for (i = 0; i < array.Length; i++)
+            {
+                for (tmp1 = i + 1; array.Length > tmp1; tmp1++)
+                {
+                    if (array[tmp1] < array[i])
+                    {
+                        tmp2 = array[i];
+                        array[i] = array[tmp1];
+                        array[tmp1] = tmp2;
+                    }
+                }
+            }
+
+
+            Console.WriteLine("Tämä ohjelma tulostaa 7 arvottua lottonumeroa korkeusjärjestyksessä.\n" +
+                "----------------------------------\n");
+            Console.ReadKey();
+
+            for (i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+
+            Console.WriteLine("\n");
+
+
+
+
+        }
+    }
+}
