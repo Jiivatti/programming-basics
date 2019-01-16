fd69382ed4a3e7fef6b0c910d6a4e1d236d41cec
Jiivatti <Jiivatti>
2019-01-16 11:39:06 +0200

    Task 1 done

---
+++ array-tasks/array-task3.1/array-task3.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace arraytask3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Console.WriteLine("Ohjelma tulostaa 10 arvottua lukua väliltä 0-20, ja lisää ylimääräisen nollan lukujen 0-9 eteen.\t");
+            Console.ReadKey();
+            Checker(array);
+        }
+
+        static void Checker(int[] arr)
+        {
+            Random rng = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                arr[i] = rng.Next(21);
+                Console.WriteLine($"Rivi {i + 1}: {arr[i]:d2}");
+            }
+        }
+    }
+}
