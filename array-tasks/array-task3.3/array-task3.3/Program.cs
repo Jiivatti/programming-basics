0ade8e269a3639571fe564c852e53688a319d84d
Jiivatti <Jiivatti>
2019-01-16 11:39:47 +0200

    Task 3 done

---
+++ array-tasks/array-task3.3/array-task3.3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace arrayTask3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[10];
+            int[] arr2 = new int[10];
+            int[] sumArray = new int[10];
+            Random rng = new Random();
+            int i = 0;
+
+            for (i = 0; i < arr1.Length; i++)
+            {
+                arr1[i] = rng.Next(1, 51);
+                arr2[i] = rng.Next(1, 51);
+            }
+
+            for (i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] < arr2[i])
+                {
+                    sumArray[i] = arr2[i];
+                }
+                else
+                {
+                    sumArray[i] = arr1[i];
+                }
+            }
+
+            Console.WriteLine("Tämä ohjelma luo kaksi taulukkoa,\nja hakee niistä indeksikohtaisesti" +
+                "\nsuurimmat arvot kolmanteen taulukkoon. \n");
+
+            Console.WriteLine("  Taulukko 1  -    Taulukko 2   -   Suurempi luku:" +
+                "\n--------------------------------------------------");
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}|    {arr1[i]:d2}        -       {arr2[i]:d2}        -       {sumArray[i]:d2}");
+            }
+
+
+        }
+    }
+}
