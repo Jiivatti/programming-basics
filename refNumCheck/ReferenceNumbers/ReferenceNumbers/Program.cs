737994d7a0730593ba58e7989f4823392f966db8
Jiivatti <Jiivatti>
2019-01-16 07:05:10 +0200

    Added some faulty input handling and comments for clarity. Still coming up with a better method of saving to file.

--- refNumCheck/ReferenceNumbers/ReferenceNumbers/Program.cs
+++ refNumCheck/ReferenceNumbers/ReferenceNumbers/Program.cs
@@ -22,10 +22,14 @@ namespace ReferenceNumbers
                         if (Checker(refNum) == false)
                         {
                             Console.WriteLine("The reference number is incorrect.\n");
+                            Console.ReadKey();
+                            Console.Clear();
                         }
                         else
                         {
                             Console.WriteLine("The reference number is correct!\n");
+                            Console.ReadKey();
+                            Console.Clear();
                         }
                         break;
 
@@ -48,11 +52,11 @@ namespace ReferenceNumbers
                 Console.WriteLine("[F] -- Close program\n");
 
                 return Console.ReadLine();
-            }
+            } // Switch casen-rakenteen kanssa toimiva käyttöliittymä
         }
 
-        public static bool Checker(string refNum)
-        {
+        public static bool Checker(string refNum) // Funktio tarkistaa parametrinä välitetyn viitenumeron tarkisteen oikeellisuuden, ja palauttaa
+        {                                         // boolean-arvon sen perusteella.
             if (refNum.Length < 3)
             {
                 Console.WriteLine("Your input was too short.");
@@ -61,6 +65,11 @@ namespace ReferenceNumbers
             else if (refNum.Length > 19)
             {
                 Console.WriteLine("Your input was too long.");
+                return false;
+            }
+            else if (refNum[0] == '0' || refNum[0] < '0')
+            {
+                Console.WriteLine("The number may not begin with 0 or less.");
                 return false;
             }
 
@@ -97,7 +106,7 @@ namespace ReferenceNumbers
             }
         }
 
-        public static void SCreator()
+        public static void SCreator() // Funktio luo tarkistenumeron käyttäjän syöttämään viitenumeron alkuosaan.
         {
             Console.Write("Input a seed consisting of 3-19 numbers to generate from: ");
             string seed = Console.ReadLine();
@@ -105,10 +114,20 @@ namespace ReferenceNumbers
             if (seed.Length > 19)
             {
                 Console.WriteLine("Your input was too long.");
+                Console.ReadKey();
+                Console.Clear();
             }
             else if (seed.Length < 3)
             {
                 Console.WriteLine("Your input was too short.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else if (seed[0] == '0' || seed[0] < '0')
+            {
+                Console.WriteLine("The number may not begin with 0 or less.");
+                Console.ReadKey();
+                Console.Clear();
             }
             else
             {
@@ -143,64 +162,94 @@ namespace ReferenceNumbers
                 }
 
                 Console.WriteLine($"Your new reference number is: {newNumb}");
+                Console.ReadKey();
+                Console.Clear();
             }
         }
-        public static void MCreator()
+        public static void MCreator() // Funktio luo käyttäjän haluaman määrän uusia viitenumeroita, ja tallentaa ne tiedostoon referencenumbers.txt
         {
             string path = @"c:\temp\referencenumbers.txt";
-            Random rng = new Random();
+            
             Console.Write("\nInput a seed consisting of 3-19 numbers to generate from: ");
             string seed = Console.ReadLine();
             Console.Write("Input the amount of reference numbers to generate: ");
             int n = Convert.ToInt32(Console.ReadLine());
 
-            for (int k = 0; k < n; k++)
+            if (seed.Length > 19)
             {
-                int g = rng.Next(10);
-                seed = seed + g;
-                int[] multiply = new int[] { 7, 3, 1 };
-                int[] refArr = new int[seed.Length];
-                int sum = 0;
-
-                for (int i = 0; i < seed.Length; i++)
-                {
-                    refArr[i] = int.Parse(seed[i].ToString());
-                }
-
-                for (int i = 0; i < refArr.Length; i++)
-                {
-                    sum += refArr[refArr.Length - 1 - i] * multiply[i % 3];
-                }
-
-                int checkSum = 10 - (sum % 10);
-                if (checkSum == 10)
-                {
-                    checkSum = 0;
-                }
-
-                string newNumb = seed + checkSum;
-
-                for (int i = 1; i < newNumb.Length; i++)
-                {
-                    if (i % 5 == 0)
-                    {
-                        newNumb = newNumb.Insert(i, " ");
-                    }
-                }
-
-                Console.WriteLine($"Number {k+1}: {newNumb}");
-
-                using (StreamWriter sw = new StreamWriter(path))
-                {
-                    for (int i = 0; i < n; i++)
-                    {
-                        sw.WriteLine($"{newNumb}   ");
-                    }
-                }
-                
+                Console.WriteLine("Your input was too long.");
+                Console.ReadKey();
+                Console.Clear();
             }
-            Console.ReadKey();
-            Console.Clear();
+            else if (seed.Length < 3)
+            {
+                Console.WriteLine("Your input was too short.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else if (seed[0] == '0' || seed[0] < '0')
+            {
+                Console.WriteLine("The number may not begin with 0 or less.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else if (n <= 0)
+            {
+                Console.WriteLine("Please input an acceptable amount of numbers to generate.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    Random rng = new Random();
+                    int g = rng.Next(10); // Keksi parempi ratkaisu, algoritmi toimii sikäli mutta rng ei uusi arvoaan jokaisella kierroksella.
+                    seed = seed + g;
+                    int[] multiply = new int[] { 7, 3, 1 };
+                    int[] refArr = new int[seed.Length];
+                    int sum = 0;
+
+                    for (int i = 0; i < seed.Length; i++)
+                    {
+                        refArr[i] = int.Parse(seed[i].ToString());
+                    }
+
+                    for (int i = 0; i < refArr.Length; i++)
+                    {
+                        sum += refArr[refArr.Length - 1 - i] * multiply[i % 3];
+                    }
+
+                    int checkSum = 10 - (sum % 10);
+                    if (checkSum == 10)
+                    {
+                        checkSum = 0;
+                    }
+
+                    string newNumb = seed + checkSum;
+
+                    for (int i = 1; i < newNumb.Length; i++)
+                    {
+                        if (i % 5 == 0)
+                        {
+                            newNumb = newNumb.Insert(i, " ");
+                        }
+                    }
+
+                    Console.WriteLine($"Number {k + 1}: {newNumb}");
+
+                    using (StreamWriter sw = new StreamWriter(path)) // Tässä muodossaan ohjelma tallentaa vain ensimmäisen generoidun numeron useaan kertaan.
+                    {
+                        for (int i = 0; i < n; i++)
+                        {
+                            sw.WriteLine($"{newNumb}   ");  // Löydä parempi tapa!
+                        }
+                    }
+
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
 
         }
     }
